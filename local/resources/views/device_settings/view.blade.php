5658ebe8923a1f559acfd31ae5ba2d91cb7f519f
bradjin8 <headit74@hotmail.com>
2025-05-20 14:55:41 -0400

    fix: style

--- local/resources/views/device_settings/view.blade.php
+++ local/resources/views/device_settings/view.blade.php
@@ -2,26 +2,25 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-2 sm:px-20 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
-                <div class="flex justify-center">
-                    <h2 class="text-xl font-extrabold text-gray-800 dark:text-white">Device Settings (Station: {{$serial}})</h2>
-                </div>
-            </div>
-            <div>
-                <table id="settings_table" class="table table-auto text-gray-800 dark:text-white bg-gray-300 dark:bg-gray-800 m-auto">
-                    <thead class="uppercase text-sm leading-normal">
+                <h2 class="text-xl font-extrabold text-gray-800 dark:text-white text-center my-3">Device Settings
+                    (Station: {{$serial}})</h2>
+                <table id="settings_table"
+                       class="table-auto md:table-fixed w-sm md:w-md lg:w-lg m-auto text-gray-800 dark:text-white bg-gray-300 dark:bg-gray-800">
+                    <thead class="uppercase text-sm leading-normal w-full">
                     <tr>
-                        <th class="border px-4 py-2">Key</th>
-                        <th class="border px-4 py-2">Value</th>
+                        <th class="border px-4 py-2 ">Key</th>
+                        <th class="border px-4 py-2 ">Value</th>
                     </tr>
                     </thead>
                     <tbody id="table_body">
                     <tr>
-                        <td class="border px-4 py-2 text-center"></td>
-                        <td class="border px-4 py-2 text-center"></td>
+                        <td class="border px-4 py-2 text-center">Key</td>
+                        <td class="border px-4 py-2 text-center">Value</td>
                     </tr>
                     </tbody>
                 </table>
             </div>
+
         </div>
     </div>
     <script>
@@ -41,7 +40,7 @@
         }
         if (html.length > 1) {
             document.getElementById('table_body').innerHTML = html;
-            document.getElementById('settings_table').style.display = 'block';
+            document.getElementById('settings_table').style.display = 'table';
         } else {
             document.getElementById('settings_table').style.display = 'none';
         }
