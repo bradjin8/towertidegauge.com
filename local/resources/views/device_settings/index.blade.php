9c5debd0697af076573ee994496ead2e3d3db80f
bradjin8 <headit74@hotmail.com>
2025-05-28 07:19:07 -0400

    fix: fix mobile design

--- local/resources/views/device_settings/index.blade.php
+++ local/resources/views/device_settings/index.blade.php
@@ -3,10 +3,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-2 sm:px-20 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
                 <h2 class="text-xl font-extrabold text-gray-800 dark:text-white text-center my-3">System Settings</h2>
-                <div class="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-8">
-                    <div class="flex flex-col justify-start items-start">
+                <div class="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-8">
+                    <div class="flex flex-col justify-center md:justify-start items-start">
                         <table id="settings_table"
-                               class="table-fixed w-sm md:w-md lg:w-lg text-gray-800 dark:text-white bg-gray-300 dark:bg-gray-800">
+                               class="table-fixed w-sm md:w-md lg:w-lg text-gray-800 dark:text-white bg-gray-300 dark:bg-gray-800 m-auto">
                             <thead class="text-sm leading-normal w-full">
                             <tr>
                                 <th class="border px-4 py-2 ">Device</th>
@@ -24,7 +24,7 @@
                             </tbody>
                         </table>
                     </div>
-                    <div class="col-span-1 md:col-span-3">
+                    <div class="md:col-span-3">
                         <pre id="json-display">
                             {test}
                         </pre>
